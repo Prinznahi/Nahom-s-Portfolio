7671e2aad54c6ae51af1257886c3ac2ad9e9ef0b
Prinznahi <nahtesh.t@gmail.com>
2024-07-02 00:30:46 -0700

    index.ts

--- data/index.ts
+++ data/index.ts
@@ -107,19 +107,19 @@ export const navItems = [
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Nahom was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nahom's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nahom is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Nahom was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nahom's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nahom is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Nahom was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nahom's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nahom is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
